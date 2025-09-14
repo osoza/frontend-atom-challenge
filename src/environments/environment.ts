9b75d7c5a99b32f0b329f94c1650442c70b49db1
Oscar Soza <osoza@trustit.cl>
2025-09-14 17:21:07 -0300

    Cambios para firestore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
    production: false,
-   apiUrl: "http://localhost:8080/api",
+   apiUrl: "http://localhost:8080",
    tokenKey: "auth_token",
    tokenExpiryKey: "auth_token_expiry"
 };
